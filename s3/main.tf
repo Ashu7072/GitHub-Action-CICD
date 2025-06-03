af39f2901e3c1d0d51d69364f77a9ff59076d76e
Ashu7072 <ashupatil2721@gmail.com>
2025-06-03 17:32:43 +0530

    updated

--- s3/main.tf
+++ s3/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "demobucket-7599"
-    key = "GitHUB-ACTION_CICD/s3"
+    key    = "GitHUB-ACTION_CICD/s3"
     region = "us-east-1"
-    dynamodb_table = "terraform-locks"    
+    use_lockfile = true    # <-- replace dynamodb_table with this
   }
 }
 
