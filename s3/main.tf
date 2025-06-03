0f3ee64d02091d97fa602078f7410c520759e847
Ashu7072 <ashupatil2721@gmail.com>
2025-06-03 16:41:08 +0530

    s3 main.tf changed

--- s3/main.tf
+++ s3/main.tf
@@ -15,3 +15,11 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-unique-s3-bucket-615516515644"
   force_destroy = true
 }
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
